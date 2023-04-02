db2b383e2cceb4cda222f563fa2c64adef86b5b5
qdelta <qdelta@qdelta.com>
2023-04-02 15:06:34 +0800

    add prompt for the worst case

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Just my workaround for this [issue](https://github.com/microsoft/vscode/issues/178425)
 
-You can rebind the key of command "File: save" to command "better-save: Save if modified"
+You can rebind the key of command "File: save" to command "better-save: Save if content changed"
--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,22 @@
 import * as vscode from 'vscode';
 
-async function betterSave() {
-    const doc = vscode.window.activeTextEditor?.document;
-    if (doc && doc.isDirty) {
+async function betterSave(currEditor: vscode.TextEditor) {
+    const doc = currEditor.document;
+    const conf = vscode.workspace.getConfiguration('better-save');
+    if (doc.isDirty) {
         const contentOnDisk = await vscode.workspace.fs.readFile(doc.uri);
         const contentHere = Buffer.from(doc.getText());
         if (! contentHere.equals(contentOnDisk)) {
             doc.save();
+        } else if (conf.get('promptDirty') as boolean) {
+            vscode.window.showInformationMessage(
+                "better-save: File content unchanged, skip (" + doc.fileName + ")"
+            );
         }
     }
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    const disposable = vscode.commands.registerCommand('better-save.save', betterSave);
+    const disposable = vscode.commands.registerTextEditorCommand('better-save.save', betterSave);
     context.subscriptions.push(disposable);
 }
